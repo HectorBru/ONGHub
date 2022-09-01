31031f09b58efa08386f08b8c295e927c578f6a5
HectorBru <brutarin3@gmail.com>
2022-09-01 23:29:29 +0200

    native android project added

---
+++ onghub-frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'hub.ONG',
+  appName: 'ONGHub',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
