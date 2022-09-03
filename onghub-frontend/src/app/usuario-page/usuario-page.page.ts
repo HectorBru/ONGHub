18ba9d9f7eac67a91437ab0d53c705fab7cb71db
HectorBru <brutarin3@gmail.com>
2022-09-03 20:02:14 +0200

    sample project finished

--- onghub-frontend/src/app/usuario-page/usuario-page.page.ts
+++ onghub-frontend/src/app/usuario-page/usuario-page.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-usuario-page',
   templateUrl: './usuario-page.page.html',
   styleUrls: ['./usuario-page.page.scss'],
 })
 export class UsuarioPagePage implements OnInit {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    await this.photoService.loadSaved();
   }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
